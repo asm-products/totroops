e76cc5509a744ec7fcccb08ebc5b257e4a32534c
phillyc <services@philcarter.me>
2014-08-30 17:03:43 -0400

    Test home view.

--- totroops/settings.py
+++ totroops/settings.py
@@ -55,12 +55,24 @@ WSGI_APPLICATION = 'totroops.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.path.join(BASE_DIR, 'db.postgresql'),
+#         'HOST': ''
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.path.join(BASE_DIR, 'db.postgresql'),
+        # 'USER': 'mydatabaseuser',
+        # 'PASSWORD': 'mypassword',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
---
+++ totroops/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+import datetime
+
+def home(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
