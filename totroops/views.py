99f3d6eaf4641cf73664dbe0e279a8cce7f1a473
phillyc <services@philcarter.me>
2014-09-01 12:57:54 -0400

    Beta signup view.

--- totroops/views.py
+++ totroops/views.py
@@ -5,3 +5,7 @@ def home(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
+
+def beta_signup(request):
+    html = "<html><body>this is the beta email signup.</body></html>"
+    return HttpResponse(html)
