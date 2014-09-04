342c13742ea2de1045c4aee979f6db197c998b04
phillyc <services@philcarter.me>
2014-09-04 13:28:01 -0400

    Basic email beta signup.

--- totroops/views.py
+++ totroops/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
 import datetime
+
 
 def home(request):
     now = datetime.datetime.now()
@@ -8,5 +10,11 @@ def home(request):
     return HttpResponse(html)
 
 def beta_signup(request):
+    if request.method == "POST":
+        # Add an entry to the auth_user table with this email address.
+        email = request.POST['email']
+        user = User.objects.create_user(username=email, email=email)
+        user.save()
+        return render(request, 'beta_signup.html')
 
     return render(request, 'beta_signup.html')
