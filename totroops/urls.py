982988bb2af8810cc89a232a4f0d4aec2544d48b
phillyc <services@philcarter.me>
2014-09-15 12:47:58 -0400

    Include favicon.

--- totroops/urls.py
+++ totroops/urls.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'totroops.views.beta_signup', name='beta_signup'),
     url(r'^thanks/$', 'totroops.views.thanks', name='thanks'),
     url(r'^faq/$', 'totroops.views.faq', name='faq'),
+
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
