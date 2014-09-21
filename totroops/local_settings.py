4ec9420edcac359b834082783fca7a21283504d5
Philip Carter <services@philcarter.me>
2014-09-21 19:51:17 -0400

    Delete local_settings.py
    Have to delete from github since this file is being ignored now.

--- totroops/local_settings.py
+++
@@ -1,8 +0,0 @@
-# Settings for local host.
-import dj_database_url
-
-DATABASES = {
-    'default': dj_database_url.config(default='postgres://ph652925:password@localhost/template1')
-}
-
-DEBUG = True
