e77da4ea882df00f8b3d31d903928ac0bd8d5603
phillyc <services@philcarter.me>
2014-09-21 15:04:44 -0400

    Django-post-office setup.

--- totroops/settings.py
+++ totroops/settings.py
@@ -32,13 +32,16 @@ PREPEND_WWW = False
 # Application definition
 
 INSTALLED_APPS = (
-    'analytical',
+    # Base django apps.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Other apps.
+    'analytical',
+    'post_office',
 )
 
 MIDDLEWARE_CLASSES = (
