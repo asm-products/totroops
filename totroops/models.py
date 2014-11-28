9d2d8f717b8a6cfce7b5753fb147a879c1878d4e
phillyc <services@philcarter.me>
2014-11-28 10:50:21 -0500

    Init order model.

--- totroops/models.py
+++ totroops/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Item(models.Model):
@@ -11,6 +12,7 @@ class Item(models.Model):
     width = models.FloatField()
     food = models.BooleanField()
 
+
 class Package(models.Model):
     items = models.ManyToManyField('Item')
     cost = models.FloatField()
@@ -22,3 +24,22 @@ class Package(models.Model):
     shipping = models.FloatField()
     price = models.FloatField()
     tax = models.FloatField()
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, blank=False, null=False)
+    destination = models.TextField()
+    packages = models.ManyToManyField('Package')
+
+    @property
+    def total_price(self):
+        """
+        Takes the sum of prices of all packages
+        :rtype: float
+        :return: Sum of price of all packages
+        """
+        total = 0;
+        for package in self.packages:
+            total += package.price
+        print type(total)
+        return total
