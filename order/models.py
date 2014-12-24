7afb019d531b85db090594cc30c541a2baecfdcb
phillyc <services@philcarter.me>
2014-12-24 12:56:21 -0500

    Add image_url and coupon to orders.

--- order/models.py
+++ order/models.py
@@ -2,24 +2,32 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Coupon(models.Model):
+    code = models.CharField(max_length=50)
+    discount = models.FloatField()
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+    valid = models.BooleanField()
+
+
 class Item(models.Model):
     name = models.CharField(max_length=50)
     source = models.TextField()
     cost = models.FloatField()
-    weight = models.FloatField()
-    height = models.FloatField()
-    length = models.FloatField()
-    width = models.FloatField()
+    weight = models.FloatField(blank=True, null=True)
+    height = models.FloatField(blank=True, null=True)
+    length = models.FloatField(blank=True, null=True)
+    width = models.FloatField(blank=True, null=True)
     food = models.BooleanField(default=False)
 
 
 class Package(models.Model):
     items = models.ManyToManyField('Item')
     cost = models.FloatField()
-    weight = models.FloatField()
-    height = models.FloatField()
-    length = models.FloatField()
-    width = models.FloatField()
+    weight = models.FloatField(blank=True, null=True)
+    height = models.FloatField(blank=True, null=True)
+    length = models.FloatField(blank=True, null=True)
+    width = models.FloatField(blank=True, null=True)
     container = models.TextField()
     shipping = models.FloatField()
     price = models.FloatField()
@@ -35,7 +43,9 @@ class Order(models.Model):
     customer = models.ForeignKey(User, blank=True, null=True)
     recipient= models.ForeignKey(Recipient, blank=False, null=False)
     packages = models.ManyToManyField('Package')
-    message = models.TextField(max_length=500, blank=True)
+    coupon = models.ForeignKey(Coupon, blank=True, null=True)
+    message = models.TextField(max_length=500, blank=True, null=True)
+    image_url = models.CharField(max_length=1000, blank=True, null=True)
 
     @property
     def total_price(self):
