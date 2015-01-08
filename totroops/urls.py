cee562b5ee9e55fda7f4134411fe2d0380fb6268
phillyc <services@philcarter.me>
2015-01-07 20:09:08 -0500

    Add static to urls.

--- totroops/urls.py
+++ totroops/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
@@ -12,4 +13,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
