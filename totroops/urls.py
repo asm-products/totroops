3bcd597c4021e628a4b3bac17df7cbebbb5d2ac1
phillyc <services@philcarter.me>
2015-01-12 12:37:51 -0500

    Refactor home page to present idea.

--- totroops/urls.py
+++ totroops/urls.py
@@ -5,10 +5,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'totroops.views.beta_signup', name='beta_signup'),
+    url(r'^$', 'totroops.views.home', name='home'),
     url(r'^thanks/$', 'totroops.views.thanks', name='thanks'),
     url(r'^faq/$', 'totroops.views.faq', name='faq'),
-    url(r'^poll/$', 'polls.views.poll', name='poll'),
 
     # url(r'^blog/', include('blog.urls')),
 
--- totroops/views.py
+++ totroops/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import RequestContext, loader
 
-from totroops.models import Recipent
+from totroops.models import Recipient
 
 import logging
 
@@ -16,31 +16,28 @@ def faq(request):
     return render(request, 'faq.html')
 
 def home(request):
-    now = datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
-
-def beta_signup(request):
     if request.method == "POST":
-        # Add an entry to the Recipent model with this info.
+        # Add an entry to the Recipient model with this info.
 
         name = request.POST['name']
         address = request.POST['address']
 
-        recipent = Recipent.get_or_create(name=name, address=address)
+        import ipdb; ipdb.set_trace()
+
+        recipient = Recipient.objects.get_or_create(name=name, address=address)
 
         # import ipdb; ipdb.set_trace()
         logger.info("%s, %s - submitted." % (name, address))
 
         template = loader.get_template('poll.html')
         context = RequestContext(request, {
-                'recipent': recipent
+                'recipient': recipient
             })
         return HttpResponse(template.render(context))
 
     data = { 'current_year': date.today().year }
 
-    return render(request, 'beta_signup.html', data)
+    return render(request, 'home.html', data)
 
 def thanks(request):
     return render(request, 'thanks.html')
